06b6a3addbd3acba762bedc377b14ed46b7b4279
Md. Moniruzzaman Khondaker Shadhin <khondaker.shadhin@konasl.com>
2022-05-22 10:57:39 +0600

    Dagger 2 Modules

---
+++ app/src/main/java/com/anushka/didemo/MemoryCardModule.kt
@@ -0,0 +1,12 @@
+package com.anushka.didemo
+
+import dagger.Module
+import dagger.Provides
+
+@Module
+class MemoryCardModule {
+    @Provides
+    fun providesMemoryCard():MemoryCard{
+        return MemoryCard()
+    }
+}
