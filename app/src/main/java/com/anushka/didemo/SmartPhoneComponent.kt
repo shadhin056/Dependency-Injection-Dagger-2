a4471a48f2bc96246d546ccf0004f703d2d22b94
Moniruzzaman shadhin <shadhinemail@gmail.com>
2022-05-24 12:55:27 +0600

    singletons object

--- app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++ app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
@@ -1,7 +1,9 @@
 package com.anushka.didemo
 
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [MemoryCardModule::class,NcBatteryModule::class])
 interface SmartPhoneComponent {
     fun inject (mainActivity:MainActivity)
